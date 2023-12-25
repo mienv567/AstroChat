9cddbb710fc4ba5488be23c55fd25c651d90dfe9
jackietan <jackietan@tencent.com>
2023-12-25 16:05:04 +0800

    dev

---
+++ test_datepicker.py
@@ -0,0 +1,44 @@
+import streamlit as st
+
+from streamlit_date_picker import date_range_picker, PickerType, Unit, date_picker
+
+st.title('Streamlit Date Picker')
+
+# Use date_range_picker to create a datetime range picker
+st.subheader('Date Range Picker')
+date_range_string = date_range_picker(picker_type=PickerType.time.string_value,
+                                      start=-30, end=0, unit=Unit.minutes.string_value,
+                                      key='range_picker',
+                                      refresh_button={'is_show': True, 'button_name': 'Refresh last 30min',
+                                                      'refresh_date': -30,
+                                                      'unit': Unit.minutes.string_value})
+if date_range_string is not None:
+    start_datetime = date_range_string[0]
+    end_datetime = date_range_string[1]
+    st.write(f"Date Range Picker [{start_datetime}, {end_datetime}]")
+
+
+
+
+
+col_date, col_time = st.columns(2)
+with col_date:
+    st.subheader('Date Picker')
+    # Use date_picker to create a date picker
+    date_string = date_picker(picker_type=PickerType.time.string_value,
+                              # unit=Unit.years.string_value,
+                              unit="minutes",
+                              key='date_picker')
+
+with col_time:
+    st.subheader('Date Picker')
+    # Use date_picker to create a date picker
+    date_string = date_picker(picker_type=PickerType.time.string_value,
+                              # unit=Unit.years.string_value,
+                              unit="minutes",
+                              key='date_picker1')
+
+
+if date_string is not None:
+    st.write('Date Picker: ', date_string)
+
